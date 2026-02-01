4c10b3b881212e8194dcb5b22ee03a8270c837ae
onasmallbit <onasmallbit@gmail.com>
2026-02-01 18:33:17 -0300

    Adapted the components to a ECS approach

--- components.lua
+++ components.lua
@@ -1,173 +1,49 @@
 --[[
 
-Este es el archivo donde se definen los componentes del juego. Cada componente
-es una tabla que contiene funciones que definen su comportamiento.
-Cada entidad puede estar suscripta a uno o más componentes, y en cada frame
-se ejecutan las funciones de los componentes a los que está suscripta.
+In this file the base components are defined. They only have data
+and in some cases helper functions.
 
 ]]--
-
-local osbmath = require "osbmath"
 
 local components = {}
 
 
--- Función para agregar un componente a una entidad.
-function components.AddComponent(entity, name, component)
-    
-    local copy = {}
-
-    for k, v in pairs(component) do
-        copy[k] = v
-    end
-
-    entity[name] = copy
-end
-
--- Función para crear una copia de un componente.
-function components.CopyComponent(component)
-    
-    local copy = {}
-
-    for k, v in pairs(component) do
-        copy[k] = v
-    end
-
-    return copy
-
-end
-
--- Función para combinar dos componentes en uno (si hay conflictos entre miembros iguales, prevalecen los
--- del componente 2).
-function components.CombineComponents(comp1, comp2)
-    
-    local combination = {}
-
-    for k, v in pairs(comp1) do
-        combination[k] = v
-    end
-
-    for k, v in pairs(comp2) do
-        combination[k] = v
-    end
-
-    return combination
-
-end
-
--------- Transform Component --------
-
--- "Transform" Define la posición y rotación de una entidad en el espacio del juego.
--- Provee funciones para mover y rotar la entidad, así como para obtener su posición actual.
-
-components.Transform = {}
-
+-- Transform: Gives entities a position vector (pointing to the upper-left corner of the box)
+-- and rotation.
 components.Transform = {
 
-    x = 0,
-    y = 0,
+    position =  {x=0, y=0},
     rotation = 0,
-
-    move = function(self, dx, dy)
-        self.x = self.x + dx
-        self.y = self.y + dy
-    end,
-
-    rotate = function(self, dtheta)
-        self.rotation = self.rotation + dtheta
-    end,
-
-    getPosition = function(self)
-        return self.x, self.y
-    end
-
 }
 
+-- Rect: Gives the entity a rectangular region related to it. 
 
--------- GraphicRect Component --------
-
--- "GraphicRect" Define una región rectangular en la pantalla propia de la entidad. Se puede
--- rellenar de un color solido, dibujar su contorno, o dibujar una imagen en su interior.
--- Nota: No tiene nada que ver con la física o las colisiones, es solo para dibujar.
--- Nota2: Requiere el componente Transform para posicionarse correctamente en pantalla.
-
-components.GraphicRect = {}
-
-components.GraphicRect = {
+components.Rect = {
 
     w = 0,
-    h = 0,
-    color = {0.8, 0, 0.2},
-
-    draw = function(self) 
-        love.graphics.setColor(self.color)
-        love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
-    end
+    h = 0
 
 }
 
 
---------- CollRect Component ---------
+-- Visual: Gives the entity a visual representation.
 
--- "CollRect" Define una región rectangular para colisiones AABB (Axis-Aligned Bounding Box).
--- Provee funciones para detectar colisiones con otras entidades que también tengan este componente.
--- Nota: Requiere el componente Transform para posicionarse correctamente en el espacio del juego.
+components.Visual = {
 
-components.CollRect = {
-
-    w = 0,
-    h = 0,
-
-    isCollidingWith = function(self, other)
-        return not (self.x + self.w < other.x or
-                    self.x > other.x + other.w or
-                    self.y + self.h < other.y or
-                    self.y > other.y + other.h)
-    end
+    color = {r=1, g=1, b=1},
+    sprite = nil
 
 }
 
---------- InvisibleBody Component ---------
+-- Cinematics: Gives the entity the capability of moving in a cinematic way.
 
--- InvisibleBody es la combinacion de un CollRect y un Transform, que permite a una entidad
--- tener posicion y colision. Evita tener que agregar los tres componentes por separado.
+components.Cinematics = {
 
-components.InvisibleBody = components.CombineComponents(
-    components.Transform,
-    components.CollRect
-)
+    velocity = {x=0, y=0},
+    acceleration = {x=0, y=0},
+    mass = 1
 
---------- Body Component ---------
-
--- Body es la combinacion de un CollRect, un GraphicRect y un Transform, que permite a una entidad
--- tener tanto colisiones como dibujo en pantalla. Evita tener que agregar los tres componentes por separado.
-
-components.Body = components.CombineComponents(
-    components.Transform,
-    components.CombineComponents(
-        components.CollRect,
-        components.GraphicRect
-    )
-)
-
---------- CinematicBody Component ---------
-
--- CinematicBody es un Body con capacidad de movimiento.
-
-components.CinematicBody = components.CopyComponent(components.Body)
-
-components.CinematicBody.vx = 0
-components.CinematicBody.vy = 0
-
--- Nota: vel_vector es un vector con campos x e y.
-components.CinematicBody.setVelocity = function(self, vel_vector)
-    self.vx = vel_vector.x
-    self.vy = vel_vector.y
-end
-
-components.CinematicBody.updatePosition = function(self, dt)
-    self.x = self.x + self.vx * dt
-    self.y = self.y + self.vy * dt
-end
+}
 
 return components
+
