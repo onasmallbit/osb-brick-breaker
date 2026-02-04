f3c10f6118a2dc58713fb9473f47290da18c1b07
onasmallbit <onasmallbit@gmail.com>
2026-02-03 22:39:25 -0300

    Some component files added

---
+++ status.lua
@@ -0,0 +1,14 @@
+-- Here is defined the status component.
+
+status = {}
+
+-- Initial positions for some entities...
+
+for i=0,118 do
+
+    local col = i % brick_cols
+    local row = i % brick_rows
+
+    status["brick" .. i] = {dead = false}
+    
+end
